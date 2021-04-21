4e46bfbc10ef2549a32b7bb8574f7207f954f4e3
Yiru Sun <yrsun@ucdavis.edu>
2021-04-20 23:53:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!"
+
+if __name__ == '__main__':
+    prot = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
