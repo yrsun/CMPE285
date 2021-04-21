0482fbb36a018cc5a2de01429069238147e058e3
Yiru Sun <yrsun@ucdavis.edu>
2021-04-21 01:09:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,14 @@ def process(symbol):
 def index():
     return render_template('home.html')
 
+@app.route("/join", method=['GET', 'POST'])
+def my_form_post():
+    symbol = request.form['symbol']
+    rslt = process(symbol)
+    result = {"output": rslt}
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
