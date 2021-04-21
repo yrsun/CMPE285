09c1a9ef4c6ac42d7b0eec48a46f6c023dc0bf32
Yiru Sun <yrsun@ucdavis.edu>
2021-04-21 01:43:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,16 +22,25 @@ def process(symbol):
     rslt += f"{regularMP} {marketPC:.02f} ({marketPCP:.02f}%)"
     return rslt
 
+def do_something(text1,text2):
+   text1 = text1.upper()
+   text2 = text2.upper()
+   combine = text1 +a text2
+   return combine
+
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('./home.html')
 
-@app.route("/join", methods=['GET', 'POST'])
+@app.route('/join', methods=['GET','POST'])
 def my_form_post():
-    symbol = request.form['symbol']
-    print(symbol)
-    rslt = process(symbol)
-    result = {"output": "ADBE"}
+    text1 = request.form['text1']
+    word = request.args.get('text1')
+    text2 = request.form['text2']
+    combine = do_something(text1,text2)
+    result = {
+        "output": combine
+    }
     result = {str(key): value for key, value in result.items()}
     return jsonify(result=result)
 
